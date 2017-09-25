402e4001e3e9ec5cfa840b5ce092d000d95cd1d9
AndrePoletto <polettoandre@gmail.com>
2017-09-25 11:12:51 -0300

    Front-end refactoring and data-error msg correction

--- python/app/static/js/home.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
-    $('.modal').modal();
-});
-
-$('#cancelAddModalBtn').on('click', function() {
-    $('#addModal').modal('close');
-});
--- python/app/static/js/passwords.js
+++ python/app/static/js/passwords.js
@@ -8,40 +8,6 @@ $('#reccode-list-passwords').on('click', function() {
     $('#recoveryPasswordRegisterModal').addClass('display-none');
 });
 
-// Adds passwordConfirm validation function
-$('#password').on('focusout', function () {
-    if ($(this).val() != $('#passwordConfirm').val()) {
-        $('#passwordConfirm').removeClass('valid').addClass('invalid');
-    } else {
-        $('#passwordConfirm').removeClass('invalid').addClass('valid');
-    }
-});
-// Adds passwordConfirm validation function
-$('#passwordConfirm').on('keyup', function () {
-    if ($(this).val() != $('#password').val()) {
-        $(this).removeClass('valid').addClass('invalid');
-    } else {
-        $(this).removeClass('invalid').addClass('valid');
-    }
-});
-
-// Adds passwordConfirm validation function
-$('#addPassword').on('focusout', function () {
-    if ($(this).val() != $('#addPasswordConfirm').val()) {
-        $('#addPasswordConfirm').removeClass('valid').addClass('invalid');
-    } else {
-        $('#addPasswordConfirm').removeClass('invalid').addClass('valid');
-    }
-});
-// Adds passwordConfirm validation function
-$('#addPasswordConfirm').on('keyup', function () {
-    if ($(this).val() != $('#addPassword').val()) {
-        $(this).removeClass('valid').addClass('invalid');
-    } else {
-        $(this).removeClass('invalid').addClass('valid');
-    }
-});
-
 // Dismiss modals
 $('#cancelEditModalBtn').on('click', function() {
     $('#editModal').modal('close');
